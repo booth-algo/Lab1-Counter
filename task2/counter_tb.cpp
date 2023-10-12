2d4d28b91f79a9fcb7b5f33b0babfe4849203f22
booth-algo <kvl01@protonmail.com>
2023-10-12 10:00:31 +0100

    did task 1 and most of task 2

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -1,115 +1,57 @@
 #include "Vcounter.h"
-
 #include "verilated.h"
-
 #include "verilated_vcd_c.h"
-
 #include "vbuddy.cpp"
 
- 
-
 int main(int argc, char **argv, char **env) {
-
-    int i;   // i counts the number of clock cycles to simulate
-
-    int clk; // clk is the module clock signal
-
- 
+    int i; // Count number of clock cycle
+    int clk; // module clock signal
 
     Verilated::commandArgs(argc, argv);
-
-    //init top verilog instance
-
-    Vcounter*top = new Vcounter;
-
+    // init top verilog instance
+    Vcounter* top = new Vcounter; // instantiate counter module as Vcounter (DUT)
     // init trace dump
-
-    Verilated::traceEverOn(true);
-
+    Verilated::traceEverOn(true); // turn on signal tracing and tell Verilator to dump waveform data to counter.vcd
     VerilatedVcdC* tfp = new VerilatedVcdC;
-
     top->trace (tfp, 99);
-
-    tfp->open ("Vcounter.vcd");
-
- 
+    tfp->open ("counter.vcd");
 
     // init Vbuddy
-
     if (vbdOpen() != 1) return(-1);
-
     vbdHeader("Lab 1: Counter");
 
- 
-
-    // initialize simulation inputs
-
+    // initialize simulation inputs (top = top-level entity, only top-level signals are visible)
     top->clk = 1;
-
     top->rst = 1;
-
     top->en = 0;
 
- 
-
     // run simulation for many clock cycles
-
-    for (i=0; i < 300 ; i++) {
-
- 
+    for (i=0; i<1000; i++){
 
         // dump variables into VCD file and toggle clock
-
-        for(clk = 0; clk < 2; clk++) {
-
-        tfp->dump (2*i+clk); // unit is in ps!!!
-
-        top->clk = !top->clk;
-
-        top->eval ();
-
+        for (clk=0; clk<2; clk++){
+            tfp->dump (2*i+clk);       // unit is in ps
+            top->clk = !top->clk;
+            top->eval ();
         }
 
- 
-
         // ++++ Send count value to Vbuddy
-
         vbdHex(4, (int(top->count) >> 16) & 0xF);
-
         vbdHex(3, (int(top->count) >> 8) & 0xF);
-
         vbdHex(2, (int(top->count) >> 4) & 0xF);
-
         vbdHex(1, int(top->count) & 0xF);
-
         vbdCycle(i+1);
-
+        // vbdPlot(int(top->count), 0, 255);
         // ---- end of Vbuddy output section
 
- 
+        top->rst = (i<2) | (i == 20);
+        // top->en = (i>4) && (i<=13 | i>=17);
+        top->en = vbdFlag();
 
-    // change rst and en signals during simulation
-
-    top->rst = (i < 2) | (i == 21);
-
-    top->en = (i > 4) && (i < 14 | i >= 17);
-
- 
-
-    if(Verilated::gotFinish()) exit(0);
-
+        if (Verilated::gotFinish()) exit(0);
     }
 
- 
-
-    vbdClose(); // ++++
-
- 
-
+    vbdClose();     // ++++
     tfp->close();
-
     exit(0);
-
- 
-
 }
